0af5b7575f9cb35529b5735aad61ec506b8186a6
edwaldkeeley <edwaldpk@gmail.com>
2022-06-02 19:19:27 +0700

    commit

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
+
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <div className=''></div>
+  )
+}
+
+export default Header
