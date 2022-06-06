c5a4eebe9bac3d231587cb7ae52c0c6ce33178b7
edwaldkeeley <edwaldpk@gmail.com>
2022-06-06 15:13:52 +0700

    commit

--- src/client.js
+++ src/client.js
@@ -2,14 +2,13 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: 'zi79te44',
-    dataset: 'production',
-    apiVersion: '2022-05-28',
-    useCdn: true,
-    token: 'skDfDysxfZ8mW9JlcPULmb5M7aiZLSK0sfooejPdWOpnz9Fca7ChIXnxsprNNrUaOsEf5LXVfE0kZPZwa1quwSx88VtxakKdScD0IG8T0g0PLFXYa4EDe83NcOvhs9JGKM3acI3nP6vZ8EdmOA4jw2ES0myJ5K1qGBIMIbsfUUetTRy4iICL'
+	projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+	dataset: 'production',
+	apiVersion: '2022-05-28',
+	useCdn: true,
+	token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
 
 export const urlFor = source => builder.image(source);
- 
