b7a83f8ff5acdb25352f8cd038c897ea80253f9f
edwaldkeeley <edwaldpk@gmail.com>
2022-06-16 09:06:55 +0700

    first commit

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
 
     return (
         <>
-            <h2 className='head-text'>chat with me</h2>
+            <h2 className='head-text'>Send a Message & chat with me</h2>
 
             <div className='app__footer-cards'>
                 <div className='app__footer-card'>
